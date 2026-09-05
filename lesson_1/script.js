b03e98c4ec121f19248fa88dbada4d9af750b363
ArmenGab <a.v.gabrielyan@gmail.com>
2026-09-05 22:06:31 +0400

    123

--- lesson_1/script.js
+++ lesson_1/script.js
@@ -9,7 +9,7 @@ const getStudentInfo = (student) => {
   let {name, age, city, skills}  = student
   let status = age >= 18 ? "Adult" : "Minor"
   console.log(`${name} is ${age} years old and lives in ${city}. Status: ${status}`)
-  skills.includes("React") ? console.log("Doesn't know React") : console.log("Knows React")
+  skills.includes("React") ? console.log("Knows React") : console.log("Doesn't know React")
 }
 
 getStudentInfo(student)
